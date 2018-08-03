5d6920f7a36cfa82b418492c66610b5eaeab14eb
ypbehere <583390908@qq.com>
2018-08-03 23:26:08 +0800

    init model

--- src/Model.cpp
+++ src/Model.cpp
@@ -1,1 +1,11 @@
 #include "Model.h"
+/************************************************************************/
+/*                         CVector                                      */
+/************************************************************************/
+CVector CVector::rotate(double angle) const
+{
+    double nowdir = dir() + angle;
+    double nowx = mod() * cos(nowdir);
+    double nowy = mod() * sin(nowdir);
+    return CVector(nowx, nowy);
+}
--- src/Model.h
+++ src/Model.h
@@ -5,54 +5,79 @@
 #include <vector>
 using namespace std;
 
-class pos {
+class CVector {
 public:
-    pos() : x_(0), y_(0) {}
-    pos(int x, int y) { x_ = x; y_ = y; }
-    double x() { return x_; }
-    double y() { return y_; }
-    void setX(double x) { x_ = x; }
-    void setY(double y) { y_ = y; }
-    pos operator+(vel v) { return pos(x_ + v.x(), y_ + v.y()); }
-    vel operator-(pos p) { return vel(x_ - p.x(), y_ - p.y()); }
+    CVector() :	_x(0), _y(0) {}
+    CVector(double x, double y) : _x(x), _y(y) {}
+    CVector(const CVector& v) :	_x(v.x()), _y(v.y()) {}
+    double mod() const { return sqrt(_x * _x + _y * _y); }
+    double dir() const { return atan2(_y, _x);}
+    CVector rotate(double angle) const;
+    double x() const { return _x; }
+    double y() const { return _y; }
+    CVector operator +(const CVector& v) const { return CVector(_x + v.x(), _y + v.y()); }
+    CVector operator -(const CVector& v) const { return CVector(_x - v.x(), _y - v.y()); }
+    CVector operator *(double a) const { return CVector(_x * a, _y * a); }
+    double operator *(CVector b) const { return double(_x * b.x() + _y * b.y()); } //向量点乘
+    CVector operator /(double a) const { return CVector(_x / a, _y / a); }
+    CVector operator -() const { return CVector(-1 * _x, -1 * _y); }
 private:
-    double x_;
-    double y_;
+    double _x, _y;
 };
 
-class vel {
+class CGeoPoint {
 public:
-    vel() : x_(0), y_(0) {}
-    vel(int x, int y) { x_ = x; y_ = y; }
-    double x() { return x_; }
-    double y() { return y_; }
-    void setX(double x) { x_ = x; }
-    void setY(double y) { y_ = y; }
-    vel operator+(vel p) { return vel(x_ + p.x(), y_ + p.y()); }
-    vel operator-(vel p) { return vel(x_ - p.x(), y_ - p.y()); }
+    CGeoPoint() : _x(0), _y(0) {}
+    CGeoPoint(double x, double y) : _x(x), _y(y) {}
+    CGeoPoint(const CGeoPoint& p) : _x(p.x()), _y(p.y()) {}
+    bool operator ==(const CGeoPoint& rhs) { return ((this->x() == rhs.x()) && (this->y() == rhs.y())); }
+    double x() const { return _x; }
+    double y() const { return _y; }
+    double dist(const CGeoPoint& p) const { return CVector(p - CGeoPoint(_x, _y)).mod(); }
+    CGeoPoint operator+(const CVector& v) const { return CGeoPoint(_x + v.x(), _y + v.y()); }
+    CGeoPoint operator*(const double& a) const { return CGeoPoint(_x * a, _y * a); }
+    CVector operator-(const CGeoPoint& p) const { return CVector(_x - p.x(), _y - p.y()); }
+    CGeoPoint midPoint(const CGeoPoint& p) const{ return CGeoPoint((_x + p.x()) / 2, (_y + p.y()) / 2); }
 private:
-    double x_;
-    double y_;
+    double _x, _y;
 };
 
-class Car {
+class Robot {
 public:
-    Car() {}
+    Robot() {}
+    CGeoPoint pos() const { return _pos; }
+    CVector vel() const { return _vel; }
+    double dir() const { return _dir; }
+    double rotVel() const { return _rotVel; }
+    void setPos(const CGeoPoint& pos) { _pos = CGeoPoint(pos); }
+    void setVel(const CVector& vel) { _vel = CVector(vel); }
+    void setDir(const double& dir) { _dir = dir; }
+    void setRotVel(const double& rotVel) { _rotVel = rotVel; }
 private:
-    pos pos_;
-    double dir_;
-    vel vel_;
-    double rotVel_;
+    CGeoPoint _pos;
+    CVector _vel;
+    double _dir;
+    double _rotVel;
 };
 
 class Obstacle {
 public:
     Obstacle() {}
+    CGeoPoint pos() const { return _pos; }
+    CVector vel() const { return _vel; }
+    void setPos(const CGeoPoint& pos) { _pos = CGeoPoint(pos); }
+    void setVel(const CVector& vel) { _vel = CVector(vel); }
 private:
-    pos pos_;
-    vel vel_;
+    CGeoPoint _pos;
+    CVector _vel;
 };
 
-vector<pos> posList;
+class RoundObstacle : private Obstacle {
+
+};
+
+class RectObstacle : private Obstacle {
+
+};
 
 #endif // MODEL_H
