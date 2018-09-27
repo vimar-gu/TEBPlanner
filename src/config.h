7ad0d29e03dd83d01ebdf0d375fda0290fa4d2e2
gjy <583390908@qq.com>
2018-09-27 20:34:49 +0800

    add annotation

--- src/config.h
+++ src/config.h
@@ -1,11 +1,13 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-const int ROBOT_NUMBER = 1;
-const int OBSTACLE_NUMBER = 10;
-const int TRAJ_POINT_NUMBER = 10;
-const double ORIGIN_X = 100;
-const double ORIGIN_Y = 100;
-const double OBSTACLE_RADIUS = 10;
+// the parameters for trajectory algorithm
+
+const int ROBOT_NUMBER = 1; // the number of our robots
+const int OBSTACLE_NUMBER = 10; // the number of obstacles
+const int TRAJ_POINT_NUMBER = 10; // the origin number of trajectory points
+const double ORIGIN_X = 100; // the x offset
+const double ORIGIN_Y = 100; // the y offset
+const double OBSTACLE_RADIUS = 10; // the size of obstacles .. wait to be modified to various ones
 
 #endif // CONFIG_H
